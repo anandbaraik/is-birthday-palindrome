81fdbf13dd36ab5c0fef5413f2e1f6674a896508
anandbaraik <anandbaraik2014@gmail.com>
2022-09-04 15:35:28 +0530

    update:logic

--- assets/app.js
+++ assets/app.js
@@ -60,6 +60,83 @@ function getAllDateFormats({day, month, year}) {
     ];
 }
 
+function isLeapYear(year) {
+
+    // leap year if perfectly divisible by 400
+    if((year%400) == 0) {
+        return true;
+    }
+
+    // leap year if perfectly divisible by 4
+    if((year%4) == 0) {
+        return true;
+    }
+
+    //not a leap year if perfectly divisible by 100
+    if((year % 100) == 0) {
+        return false;
+    }
+
+    //everything else : not a leap year
+    return false;
+}
+
+function getNextDate(date) {
+    let day = date.day + 1;
+    let month = date.month;
+    let year = date.year;
+
+    const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    //if feb: check leap year
+    if(month == 2){
+        if(isLeapYear(year)) {
+            if(day > 29) {
+                day = 1;
+                month = 3;
+            }
+        } else {
+            if(day > 28) {
+                day = 1;
+                month = 3;
+            }
+        }
+    } else { //else rest
+        if(day > daysInMonth[month - 1]) {
+            day = 1;
+            month ++;
+        }
+    }
+
+    //if month > 12
+    if(month > 12) {
+        month = 1;
+        year ++;
+    }
+
+    return {
+        day: day,
+        month: month,
+        year: year
+    }
+}
+
+function getNextPalindromeDate(date) {
+    let nextDate = getNextDate(date);
+    let counter = 0;
+    while(1) {
+        counter++;
+        let dateStr = convertDateToStr(nextDate);
+        let allDateFormats = getAllDateFormats(dateStr);
+        for (let index = 0; index < allDateFormats.length; index++) {
+            if(isPalindrome(allDateFormats[index])) {
+                return [counter, dateStr];
+            }
+        }
+        nextDate = getNextDate(nextDate);
+    }
+}
+
 function checkIfDateisPalindrome(dob) {
     let date = convertDateToStr(dob);
     let allDateFormatArr = getAllDateFormats(date);
@@ -75,7 +152,8 @@ function checkIfDateisPalindrome(dob) {
     if(isPalindromeFlag) {
         showMessage("Woah! your birthdate is palindrome!");
     } else {
-        showMessage("missed");
+        let [missedBy, nextDate] = getNextPalindromeDate(dob);
+        showMessage(`The nearest palindrome date is ${nextDate.day}-${nextDate.month}-${nextDate.year}, you missed by ${missedBy} days.`);
     }
 }
 
